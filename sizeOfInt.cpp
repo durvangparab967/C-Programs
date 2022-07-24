fc714769e2ba39fb12913a8c9ae99d2b4891fe6a
durvangparab967 <73769213+durvangparab967@users.noreply.github.com>
2022-07-24 13:16:20 +0530

    size of

---
+++ sizeOfInt.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a;
+    cout<<"size of integer is: "<<sizeof(a)<<endl;
+    long int b;
+    cout<<"size of long integer is: "<<sizeof(b)<<endl;
+    short int c;
+    cout<<"size of short integer is: "<<sizeof(c)<<endl;
+    float d;
+    cout<<"size of float is: "<<sizeof(d)<<endl;
+    double e;
+    cout<<"size of double is: "<<sizeof(e)<<endl;
+
+}
